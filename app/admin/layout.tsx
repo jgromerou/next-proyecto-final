e50f852894eb6b0ec4b4d540a185a97e872fc4fd
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2025-10-22 22:27:16 -0300

    se puso el force dynamic en el layout de admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,5 @@
+export const dynamic = "force-dynamic";
 import AdminSidebar from "@/components/admin/AdminSidebar";
-
 
 export default async function AdminLayout({
     children,
--- app/admin/materias/page.tsx
+++ app/admin/materias/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = "force-dynamic";
+
 
 import Heading from "@/components/ui/Heading";
 import Link from "next/link";
